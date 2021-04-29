96f9bcf170b6b389fb0cab96c0d5857d148d9056
zenan.fu <dantefu9001@163.com>
2021-04-29 09:56:46 +0800

    remove welcome module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/equipments' },
-  // { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'equipments', loadChildren: () => import('./pages/equipments/equipments.module').then(m => m.EquipmentsModule) }
 ];
 
--- src/app/pages/equipments/equipments.module.ts
+++ src/app/pages/equipments/equipments.module.ts
@@ -5,7 +5,6 @@ import { EquipmentsRoutingModule } from './equipments-routing.module';
 import { EquipmentsComponent } from './equipments.component';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzFormModule } from 'ng-zorro-antd/form';
-import { WelcomeModule } from '../welcome/welcome.module';
 import { EquipmentSearchComponent } from './equipment-search/equipment-search.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -21,8 +20,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule,
     NzTableModule,
     NzFormModule,
-    EquipmentsRoutingModule,
-    WelcomeModule
+    EquipmentsRoutingModule
   ]
 })
 export class EquipmentsModule { }
