fd4fb468a3e8906ae5b7ea0984484b3a31af03ad
zenan <dantefu9001@163.com>
2021-05-31 14:48:57 +0800

    修改表头

--- src/app/pages/equipments-maintenance-simple/equipments-maintenance-record/equipment-maintenance-record-edit/equipment-maintenance-record-edit.component.ts
+++ src/app/pages/equipments-maintenance-simple/equipments-maintenance-record/equipment-maintenance-record-edit/equipment-maintenance-record-edit.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Area, Person, Process, Workshop} from "../../../equipments/model/model";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-equipment-maintenance-record-edit',
   templateUrl: './equipment-maintenance-record-edit.component.html'
 })
-export class EquipmentMaintenanceRecordEditComponent {
+export class EquipmentMaintenanceRecordEditComponent implements OnInit{
   isVisible = false;
   isOkLoading = false;
+  selectedPerson!: Person;
+  personnel!: Array<Person>;
+  selectedWorkshops!: Workshop;
+  workshops!: Array<Workshop>;
+  selectedArea!: Area;
+  areas!: Array<Area>;
+  selectedProcess!: Process;
+  process!: Array<Process>;
+  description!: string;
+  equipmentMaintenanceEditForm!: FormGroup;
 
   showModal(): void {
     this.isVisible = true;
@@ -22,5 +34,21 @@ export class EquipmentMaintenanceRecordEditComponent {
 
   handleCancel(): void {
     this.isVisible = false;
+  }
+
+  constructor(public fb:FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.equipmentMaintenanceEditForm = this.fb.group({
+      code:new FormControl(''),
+      personnel: new FormControl(''),
+      workshop: new FormControl(''),
+      area:new FormControl(''),
+      process: new FormControl(''),
+      equipment: new FormControl(''),
+      malfunctionTime: [null],
+      description: new FormControl('')
+    })
   }
 }
